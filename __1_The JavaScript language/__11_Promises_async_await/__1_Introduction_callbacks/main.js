3cf3c42f7fc85278efc01773138523cb2516c54a
zelitosaide <zelitosaide83@gmail.com>
2022-11-01 22:51:15 +0200

    callback

--- __1_The JavaScript language/__11_Promises_async_await/__1_Introduction_callbacks/main.js
+++ __1_The JavaScript language/__11_Promises_async_await/__1_Introduction_callbacks/main.js
@@ -15,6 +15,21 @@ label02: {
     script.src = src;
     document.head.append(script);
   }
-  loadScript("./script_1.js");
-  print("2. Something here...");
+  // loadScript("./script_1.js");
+  // print("2. Something here...");
+}
+
+label03: {
+  const loadScript = function (src) {
+    const script = document.createElement("script");
+    script.src = src;
+    document.head.append(script);
+  }
+  loadScript("./script_2.js");
+
+  try {
+    newFunction();  // no such function why????
+  } catch (error) {
+    print(error.message);
+  }
 }
---
+++ __1_The JavaScript language/__11_Promises_async_await/__1_Introduction_callbacks/script_2.js
@@ -0,0 +1,3 @@
+function newFunction() {
+  console.log("New Function running...");
+}
