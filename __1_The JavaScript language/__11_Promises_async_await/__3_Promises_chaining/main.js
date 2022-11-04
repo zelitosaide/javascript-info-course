7579d896d4b30f1046e2e5dadd77cf544ea8f1d8
zelitosaide <zelitosaide83@gmail.com>
2022-11-04 13:40:38 +0200

    Promises chaining

--- __1_The JavaScript language/__11_Promises_async_await/__3_Promises_chaining/main.js
+++ __1_The JavaScript language/__11_Promises_async_await/__3_Promises_chaining/main.js
@@ -26,19 +26,46 @@ label02: {
     setTimeout(function () { resolve(1); }, 1000);
   });
 
-  promise.then(function (result) {
-    print(result);
-    return result * 2;
+  // promise.then(function (result) {
+  //   print(result);
+  //   return result * 2;
+  // });
+
+  // promise.then(function (result) {
+  //   print(result);
+  //   return result * 2;
+  // });
+
+  // promise.then(function (result) {
+  //   print(result);
+  //   return result * 2;
+  // });
+}
+
+label03: {
+  const promise = new Promise(function (resolve) {
+    setTimeout(function () { resolve(1); }, 1000);
   });
 
-  promise.then(function (result) {
-    print(result);
-    return result * 2;
-  });
+  promise
+    .then(function (result) {
+      print(result);
 
-  promise.then(function (result) {
-    print(result);
-    return result * 2;
-  });
+      return new Promise(function (resolve) {
+        setTimeout(function () { resolve(result * 2); }, 1000);
+      });
+    })
+    .then(function (result) {
+      print(result);
+
+      return new Promise(function (resolve) {
+        setTimeout(function () { resolve(result * 2); }, 1000);
+      });
+    })
+    .then(function (result) {
+
+      print(result);
+
+    });
 }
 
