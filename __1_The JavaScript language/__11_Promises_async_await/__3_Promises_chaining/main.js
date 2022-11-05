8be4e7f30a5be82e0217d754709e1f08c8b305eb
zelitosaide <zelitosaide83@gmail.com>
2022-11-05 18:29:51 +0200

    Promises chaining

--- __1_The JavaScript language/__11_Promises_async_await/__3_Promises_chaining/main.js
+++ __1_The JavaScript language/__11_Promises_async_await/__3_Promises_chaining/main.js
@@ -45,51 +45,47 @@ label02: {
 }
 
 label03: {
-  const promise = new Promise(function (resolve) {
-    setTimeout(function () {
-      resolve(1);
-    }, 1000);
-  });
-
-  promise
-    .then(function (result) {
-      print(result);
-
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          resolve(result * 2);
-        }, 1000);
-      });
-    })
-    .then(function (result) {
-      print(result);
-
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          resolve(result * 2);
-        }, 1000);
-      });
-    })
-    .then(function (result) {
-      print(result);
-    });
+  // const promise = new Promise(function (resolve) {
+  //   setTimeout(function () {
+  //     resolve(1);
+  //   }, 1000);
+  // });
+  // promise
+  //   .then(function (result) {
+  //     print(result);
+  //     return new Promise(function (resolve) {
+  //       setTimeout(function () {
+  //         resolve(result * 2);
+  //       }, 1000);
+  //     });
+  //   })
+  //   .then(function (result) {
+  //     print(result);
+  //     return new Promise(function (resolve) {
+  //       setTimeout(function () {
+  //         resolve(result * 2);
+  //       }, 1000);
+  //     });
+  //   })
+  //   .then(function (result) {
+  //     print(result);
+  //   });
 }
 
 label04: {
-  const loadScript = function (src) {
-    return new Promise(function (resolve, reject) {
-      const script = document.createElement("script");
-      script.src = src;
-      script.onload = function () {
-        resolve(script);
-      };
-      script.onerror = function () {
-        reject(new Error(`Script load error for ${src}`));
-      };
-      document.head.append(script);
-    });
-  };
-
+  // const loadScript = function (src) {
+  //   return new Promise(function (resolve, reject) {
+  //     const script = document.createElement("script");
+  //     script.src = src;
+  //     script.onload = function () {
+  //       resolve(script);
+  //     };
+  //     script.onerror = function () {
+  //       reject(new Error(`Script load error for ${src}`));
+  //     };
+  //     document.head.append(script);
+  //   });
+  // };
   // loadScript("./script1.js")
   //   .then(function (script) {
   //     one();
@@ -108,23 +104,21 @@ label04: {
 }
 
 label05: {
-  const promise = new Promise(function (resolve, reject) {
-    const script = document.createElement("script");
-    script.src = "./script1.js";
-    script.onload = function () {
-      resolve(script);
-    };
-    script.onerror = function () {
-      reject(new Error(`Script load error for ./script1.js`));
-    };
-    document.head.append(script);
-  });
-
+  // const promise = new Promise(function (resolve, reject) {
+  //   const script = document.createElement("script");
+  //   script.src = "./script1.js";
+  //   script.onload = function () {
+  //     resolve(script);
+  //   };
+  //   script.onerror = function () {
+  //     reject(new Error(`Script load error for ./script1.js`));
+  //   };
+  //   document.head.append(script);
+  // });
   // promise
   //   .then(function (script) {
   //     one();
   //     print(script);
-
   //     return new Promise(function (resolve, reject) {
   //       const script = document.createElement("script");
   //       script.src = "./script2.js";
@@ -140,7 +134,6 @@ label05: {
   //   .then(function (script) {
   //     two();
   //     print(script);
-
   //     return new Promise(function (resolve, reject) {
   //       const script = document.createElement("script");
   //       script.src = "./script3.js";
@@ -154,28 +147,25 @@ label05: {
   //     });
   //   })
   //   .then(function (script) {
-
   //     three();
   //     print(script);
-
   //   });
 }
 
 label04_1: {
-  const loadScript = function (src) {
-    return new Promise(function (resolve, reject) {
-      const script = document.createElement("script");
-      script.src = src;
-      script.onload = function () {
-        resolve(script);
-      };
-      script.onerror = function () {
-        reject(new Error(`Script load error for ${src}`));
-      };
-      document.head.append(script);
-    });
-  };
-
+  // const loadScript = function (src) {
+  //   return new Promise(function (resolve, reject) {
+  //     const script = document.createElement("script");
+  //     script.src = src;
+  //     script.onload = function () {
+  //       resolve(script);
+  //     };
+  //     script.onerror = function () {
+  //       reject(new Error(`Script load error for ${src}`));
+  //     };
+  //     document.head.append(script);
+  //   });
+  // };
   // loadScript("./script1.js")
   //   .then(function (script) {
   //     return loadScript("./script2.js");
@@ -191,54 +181,108 @@ label04_1: {
 }
 
 label04_1: {
-  const loadScript = function (src) {
-    return new Promise(function (resolve, reject) {
-      const script = document.createElement("script");
-      script.src = src;
-      script.onload = function () {
-        resolve(script);
-      };
-      script.onerror = function () {
-        reject(new Error(`Script load error for ${src}`));
-      };
-      document.head.append(script);
-    });
-  };
-
-  loadScript("./script1.js")
-    .then((script) => loadScript("./script2.js"))
-    .then((script) => loadScript("./script3.js"))
-    .then((script) => {
-      // scripts are loaded, we can use functions declared there
-      one();
-      two();
-      three();
-    });
+  // const loadScript = function (src) {
+  //   return new Promise(function (resolve, reject) {
+  //     const script = document.createElement("script");
+  //     script.src = src;
+  //     script.onload = function () {
+  //       resolve(script);
+  //     };
+  //     script.onerror = function () {
+  //       reject(new Error(`Script load error for ${src}`));
+  //     };
+  //     document.head.append(script);
+  //   });
+  // };
+  // loadScript("./script1.js")
+  //   .then((script) => loadScript("./script2.js"))
+  //   .then((script) => loadScript("./script3.js"))
+  //   .then((script) => {
+  //     // scripts are loaded, we can use functions declared there
+  //     one();
+  //     two();
+  //     three();
+  //   });
 }
 
 label5: {
-  const loadScript = function (src) {
-    return new Promise(function (resolve, reject) {
-      const script = document.createElement("script");
-      script.src = src;
-      script.onload = function () {
-        resolve(script);
-      };
-      script.onerror = function () {
-        reject(new Error(`Script load error for ${src}`));
-      };
-      document.head.append(script);
-    });
-  };
+  // const loadScript = function (src) {
+  //   return new Promise(function (resolve, reject) {
+  //     const script = document.createElement("script");
+  //     script.src = src;
+  //     script.onload = function () {
+  //       resolve(script);
+  //     };
+  //     script.onerror = function () {
+  //       reject(new Error(`Script load error for ${src}`));
+  //     };
+  //     document.head.append(script);
+  //   });
+  // };
+  // loadScript("./script1.js").then(function (script1) {
+  //   loadScript("./script2.js").then(function (script2) {
+  //     loadScript("./script3.js").then(function (script3) {
+  //       one();
+  //       two();
+  //       three();
+  //       print(script1, script2, script3);
+  //     });
+  //   });
+  // });
+}
 
-  loadScript("./script1.js").then(function (script1) {
-    loadScript("./script2.js").then(function (script2) {
-      loadScript("./script3.js").then(function (script3) {
-        one();
-        two();
-        three();
-        print(script1, script2, script3);
-      });
-    });
+label06: {
+  // class Thenable {
+  //   constructor(num) {
+  //     this.num = num;
+  //   }
+  //   then(resolve, reject) {
+  //     setTimeout(() => {
+  //       resolve(this.num * 2);
+  //     }, 1000);
+  //   }
+  // }
+  // const promise = new Promise(function (resolve) {
+  //   resolve(1);
+  // });
+  // promise
+  //   .then(function (result) {
+  //     return new Thenable(result);
+  //   })
+  //   .then(function (result) {
+  //     print(result);
+  //   });
+}
+
+label07: {
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(10);
+    }, 1000);
   });
+
+  promise
+    .then(function (result) {
+      return {
+        then(resolve, reject) {
+          print(result);
+          setTimeout(function () {
+            resolve(Math.pow(result, 2));
+          }, 1000);
+        },
+      };
+    })
+    .then(function (result) {
+      return {
+        then(resolve, reject) {
+          print(result);
+          setTimeout(function () {
+            resolve(Math.pow(result, 2));
+          }, 1000);
+        },
+      };
+    })
+    .then(function (result) {
+      print(result);
+    });
 }
