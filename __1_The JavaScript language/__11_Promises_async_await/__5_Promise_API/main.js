d9a6805a1191983cc414bcb537e3176c6936794d
zelitosaide <zelitosaide83@gmail.com>
2022-11-11 21:35:39 +0200

    Promise API

--- __1_The JavaScript language/__11_Promises_async_await/__5_Promise_API/main.js
+++ __1_The JavaScript language/__11_Promises_async_await/__5_Promise_API/main.js
@@ -30,9 +30,36 @@ label02: {
     return fetch(url);
   });
 
+  // Promise.all waits until all jobs are resolved
   Promise.all(requests).then(function (responses) {
     responses.forEach(function (response) {
       console.log(`${response.url}: ${response.status}`);
     });
   });
+}
+
+label03: {
+  const urls = [
+    "https://api.github.com/users/iliakan",
+    "https://api.github.com/users/remy",
+    "https://api.github.com/users/jeresig",
+  ];
+
+  const requests = urls.map(function (url) {
+    return fetch(url);
+  });
+
+  Promise.all(requests)
+    .then(function (responses) {
+      return Promise.all(
+        responses.map(function (response) {
+          return response.json();
+        })
+      );
+    })
+    .then(function (users) {
+      users.forEach(function (user) {
+        console.log(user.name);
+      });
+    });
 }
