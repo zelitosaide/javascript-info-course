d8401004ec71eb08abfbb432912b3a9bcae7ff23
zelitosaide <zelitosaide83@gmail.com>
2022-11-12 10:29:51 +0200

    Promise API

--- __1_The JavaScript language/__11_Promises_async_await/__5_Promise_API/main.js
+++ __1_The JavaScript language/__11_Promises_async_await/__5_Promise_API/main.js
@@ -1,97 +1,113 @@
-label01: {
-  Promise.all([
-    new Promise(function (resolve) {
-      setTimeout(function () {
-        resolve(1);
-      }, 3000);
-    }),
-    new Promise(function (resolve) {
-      setTimeout(function () {
-        resolve(2);
-      }, 2000);
-    }),
-    new Promise(function (resolve) {
-      setTimeout(function () {
-        resolve(3);
-      }, 1000);
-    }),
-  ]).then(console.log);
-}
+// label01: {
+//   Promise.all([
+//     new Promise(function (resolve) {
+//       setTimeout(function () {
+//         resolve(1);
+//       }, 3000);
+//     }),
+//     new Promise(function (resolve) {
+//       setTimeout(function () {
+//         resolve(2);
+//       }, 2000);
+//     }),
+//     new Promise(function (resolve) {
+//       setTimeout(function () {
+//         resolve(3);
+//       }, 1000);
+//     }),
+//   ]).then(console.log);
+// }
 
-label02: {
+// label02: {
+//   const urls = [
+//     "https://api.github.com/users/iliakan",
+//     "https://api.github.com/users/remy",
+//     "https://api.github.com/users/jeresig",
+//   ];
+
+//   // map every url to the promise of the fetch
+//   const requests = urls.map(function (url) {
+//     return fetch(url);
+//   });
+
+//   // Promise.all waits until all jobs are resolved
+//   Promise.all(requests).then(function (responses) {
+//     responses.forEach(function (response) {
+//       console.log(`${response.url}: ${response.status}`);
+//     });
+//   });
+// }
+
+// label03: {
+//   const urls = [
+//     "https://api.github.com/users/iliakan",
+//     "https://api.github.com/users/remy",
+//     "https://api.github.com/users/jeresig",
+//   ];
+
+//   const requests = urls.map(function (url) {
+//     return fetch(url);
+//   });
+
+//   Promise.all(requests)
+//     .then(function (responses) {
+//       return Promise.all(
+//         responses.map(function (response) {
+//           return response.json();
+//         })
+//       );
+//     })
+//     .then(function (users) {
+//       users.forEach(function (user) {
+//         console.log(user.name);
+//       });
+//     });
+// }
+
+// label04: {
+//   Promise.all([
+//     new Promise(function (resolve, reject) {
+//       setTimeout(function () {
+//         resolve(1);
+//       }, 1000);
+//     }),
+//     new Promise(function (resolve, reject) {
+//       setTimeout(function () {
+//         reject(new Error("Whoops!"));
+//       }, 2000);
+//     }),
+//     new Promise(function (resolve, reject) {
+//       setTimeout(function () {
+//         resolve(3);
+//       }, 3000);
+//     }),
+//   ]).catch(console.log);
+// }
+
+// label05: {
+//   Promise.all([
+//     new Promise(function (resolve, reject) {
+//       setTimeout(function () {
+//         resolve(1);
+//       }, 1000);
+//     }),
+//     2,
+//     3,
+//   ]).then(console.log);
+// }
+
+label06: {
   const urls = [
     "https://api.github.com/users/iliakan",
     "https://api.github.com/users/remy",
-    "https://api.github.com/users/jeresig",
+    "https://no-such-url",
   ];
 
-  // map every url to the promise of the fetch
   const requests = urls.map(function (url) {
     return fetch(url);
   });
 
-  // Promise.all waits until all jobs are resolved
-  Promise.all(requests).then(function (responses) {
-    responses.forEach(function (response) {
-      console.log(`${response.url}: ${response.status}`);
-    });
-  });
-}
+  Promise.all(requests).then(console.log).catch(console.log);
 
-label03: {
-  const urls = [
-    "https://api.github.com/users/iliakan",
-    "https://api.github.com/users/remy",
-    "https://api.github.com/users/jeresig",
-  ];
-
-  const requests = urls.map(function (url) {
-    return fetch(url);
-  });
-
-  Promise.all(requests)
-    .then(function (responses) {
-      return Promise.all(
-        responses.map(function (response) {
-          return response.json();
-        })
-      );
-    })
-    .then(function (users) {
-      users.forEach(function (user) {
-        console.log(user.name);
-      });
-    });
-}
-
-label04: {
-  Promise.all([
-    new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        resolve(1);
-      }, 1000);
-    }),
-    new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        reject(new Error("Whoops!"));
-      }, 2000);
-    }),
-    new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        resolve(3);
-      }, 3000);
-    }),
-  ]).catch(console.log);
-}
-
-label05: {
-  Promise.all([
-    new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        resolve(1);
-      }, 1000);
-    }),
-    2,
-    3,
-  ]).then(console.log);
+  Promise.allSettled(requests).then(console.log);
 }
