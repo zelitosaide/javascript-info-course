f6db941e72dd50af22a702b2f43aadfc95b83c1d
zelitosaide <zelitosaide83@gmail.com>
2022-12-08 23:49:19 +0200

    Async/await

--- __1_The JavaScript language/__11_Promises_async_await/__8_Async_await/main.js
+++ __1_The JavaScript language/__11_Promises_async_await/__8_Async_await/main.js
@@ -11,3 +11,18 @@ label02: {
   };
   func().then(console.log);
 }
+
+label03: {
+  const promise = new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(3);
+    }, 1000);
+  });
+
+  const func = async function () {
+    const result = await promise;
+    console.log(result);
+  };
+
+  func();
+}
