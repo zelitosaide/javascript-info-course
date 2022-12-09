ac95dda59005b8ac53ed3d903e650b87469c79f1
zelitosaide <zelitosaide83@gmail.com>
2022-12-09 13:17:49 +0200

    Async/await

--- __1_The JavaScript language/__11_Promises_async_await/__8_Async_await/main.js
+++ __1_The JavaScript language/__11_Promises_async_await/__8_Async_await/main.js
@@ -1,64 +1,64 @@
 import { user } from "./script.js";
 
-// label01: {
-//   const func = async function () {
-//     return 1;
-//   };
-//   func().then(console.log);
-// }
+label01: {
+  const func = async function () {
+    return 1;
+  };
+  func().then(console.log);
+}
 
-// label02: {
-//   const func = async function () {
-//     return Promise.resolve(2);
-//   };
-//   func().then(console.log);
-// }
+label02: {
+  const func = async function () {
+    return Promise.resolve(2);
+  };
+  func().then(console.log);
+}
 
-// label03: {
-//   const promise = new Promise(function (resolve) {
-//     setTimeout(function () {
-//       resolve(3);
-//     }, 1000);
-//   });
+label03: {
+  const promise = new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(3);
+    }, 1000);
+  });
 
-//   const func = async function () {
-//     const result = await promise;
-//     console.log(result);
-//   };
+  const func = async function () {
+    const result = await promise;
+    console.log(result);
+  };
 
-//   func();
-// }
+  func();
+}
 
-// label04: {
-//   const showAvatar = async function () {
-//     // read our JSON
-//     const response = await fetch("user.json");
-//     const user = await response.json();
+label04: {
+  const showAvatar = async function () {
+    // read our JSON
+    const response = await fetch("user.json");
+    const user = await response.json();
 
-//     // read github user
-//     const githubResponse = await fetch(
-//       `https://api.github.com/users/${user.username}`
-//     );
-//     const githubUser = await githubResponse.json();
+    // read github user
+    const githubResponse = await fetch(
+      `https://api.github.com/users/${user.username}`
+    );
+    const githubUser = await githubResponse.json();
 
-//     // show the avatar
-//     const img = document.createElement("img");
-//     img.src = githubUser.avatar_url;
-//     img.style.width = "100px";
-//     document.body.append(img);
+    // show the avatar
+    const img = document.createElement("img");
+    img.src = githubUser.avatar_url;
+    img.style.width = "100px";
+    document.body.append(img);
 
-//     // wait 3 seconds
-//     await new Promise(function (resolve) {
-//       setTimeout(resolve, 3000);
-//     });
+    // wait 3 seconds
+    await new Promise(function (resolve) {
+      setTimeout(resolve, 3000);
+    });
 
-//     img.remove();
+    img.remove();
 
-//     return showAvatar;
-//   };
+    return showAvatar;
+  };
 
-//   showAvatar();
-// }
+  showAvatar();
+}
 
 label05: {
   console.log(user);
