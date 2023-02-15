40009e3c99599c4f9eca2ea7163eb9ce714ffeb0
zelitosaide <zelitosaide83@gmail.com>
2023-02-15 17:50:47 +0200

    Document

--- part-2-browser-document-events-interfaces/document/browser-environment-specs/index.js
+++ part-2-browser-document-events-interfaces/document/browser-environment-specs/index.js
@@ -7,3 +7,17 @@ window.sayHi();
 
 // And we can use it as a browser window, to show the window height:
 alert(window.innerHeight);
+
+document.body.style.backgroundColor = "pink";
+
+setTimeout(function () {
+  document.body.style.backgroundColor = "brown";
+}, 1000);
+
+alert(location.href);
+
+if (confirm("Go to Wikipedia?")) {
+  location.href = "https://wikipedia.org";
+}
+
+// The functions alert/confirm/prompt are also a part of the BOM
