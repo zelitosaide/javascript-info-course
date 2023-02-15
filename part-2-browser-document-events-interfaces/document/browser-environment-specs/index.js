f994fa66b87a4bc2f48ed42d7a9c8214110aa38e
zelitosaide <zelitosaide83@gmail.com>
2023-02-15 10:51:09 +0200

    Document

--- part-2-browser-document-events-interfaces/document/browser-environment-specs/index.js
+++ part-2-browser-document-events-interfaces/document/browser-environment-specs/index.js
@@ -4,3 +4,6 @@ function sayHi() {
 
 // global functions are methods of the global object:
 window.sayHi();
+
+// And we can use it as a browser window, to show the window height:
+alert(window.innerHeight);
