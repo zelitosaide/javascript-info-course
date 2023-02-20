7cd0ef893b550575ffaf340ad6efd16bd18a7b42
zelitosaide <zelitosaide83@gmail.com>
2023-02-20 21:27:49 +0200

    Document

--- part-2-browser-document-events-interfaces/document/walking-the-dom/index.js
+++ part-2-browser-document-events-interfaces/document/walking-the-dom/index.js
@@ -1,3 +1,7 @@
 const html = document.documentElement;
 const body = document.body;
 const head = document.head;
+
+for (let i = 0; i < body.childNodes.length; i++) {
+  alert(body.childNodes[i]);
+}
