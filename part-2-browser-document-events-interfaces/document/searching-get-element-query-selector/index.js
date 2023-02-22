2e93a268f6910b7f40fee7068248413ea32db2bc
zelitosaide <zelitosaide83@gmail.com>
2023-02-22 21:42:56 +0200

    Document

--- part-2-browser-document-events-interfaces/document/searching-get-element-query-selector/index.js
+++ part-2-browser-document-events-interfaces/document/searching-get-element-query-selector/index.js
@@ -1,7 +1,14 @@
 // const elem = document.getElementById("elem");
 // elem.style.backgroundColor = "red";
 
+// That’s unless we declare a JavaScript variable with the same name, then it takes precedence:
+const elem = 5;
+console.log(elem);
+
 // or
-elem.style.backgroundColor = "green";
+if (elem instanceof Object) {
+  elem.style.backgroundColor = "green";
+}
+
 const elemContent = window["elem-content"];
 elemContent.style.color = "white";
