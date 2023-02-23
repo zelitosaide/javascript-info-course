2aa786d9b65bbf02a5a0445f10d9761e68a84b48
zelitosaide <zelitosaide83@gmail.com>
2023-02-23 08:18:50 +0200

    Document

--- part-2-browser-document-events-interfaces/document/searching-get-element-query-selector/index.js
+++ part-2-browser-document-events-interfaces/document/searching-get-element-query-selector/index.js
@@ -24,3 +24,7 @@ console.log(elements2);
 for (const el of elements) {
   console.log(el.innerHTML);
 }
+
+// document.querySelectorAll(':hover')
+
+// will return the collection with elements that the pointer is over now (in nesting order: from the outermost <html> to the most nested one).
