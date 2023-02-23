dbf3fc40e084cb18689ca2de7d42953a764886f2
zelitosaide <zelitosaide83@gmail.com>
2023-02-23 08:25:20 +0200

    Document

--- part-2-browser-document-events-interfaces/document/searching-get-element-query-selector/index.js
+++ part-2-browser-document-events-interfaces/document/searching-get-element-query-selector/index.js
@@ -32,4 +32,4 @@ console.log(elements3, elements);
 
 // will return the collection with elements that the pointer is over now (in nesting order: from the outermost <html> to the most nested one).
 const li1 = document.querySelector("ul > li:last-child");
-console.log(li1);
+console.log(li1 === elements[0]);
