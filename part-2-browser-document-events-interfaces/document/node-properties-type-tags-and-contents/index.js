019336fab317c0cd51dea78b8496cd5fd7f894e8
zelitosaide <zelitosaide83@gmail.com>
2023-02-24 14:26:06 +0200

    Document

--- part-2-browser-document-events-interfaces/document/node-properties-type-tags-and-contents/index.js
+++ part-2-browser-document-events-interfaces/document/node-properties-type-tags-and-contents/index.js
@@ -9,3 +9,4 @@ console.log(document.documentElement.constructor.name);
 console.log(document.body instanceof HTMLBodyElement);
 console.log(document.body instanceof HTMLElement);
 console.log(document.body instanceof Element);
+console.log(document.body instanceof Node);
