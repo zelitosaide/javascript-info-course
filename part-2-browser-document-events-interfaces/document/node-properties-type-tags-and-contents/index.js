91c6e5338fc1c1d1d7a6621090c7fc22cacd4d49
zelitosaide <zelitosaide83@gmail.com>
2023-02-25 12:48:36 +0200

    Document

--- part-2-browser-document-events-interfaces/document/node-properties-type-tags-and-contents/index.js
+++ part-2-browser-document-events-interfaces/document/node-properties-type-tags-and-contents/index.js
@@ -12,4 +12,4 @@ console.log(document.body instanceof Element);
 console.log(document.body instanceof Node);
 console.log(document.body instanceof EventTarget);
 
-console.dir(document.body.prototype);
+console.log(document.body.__proto__);
