7fe47edf79bb73b053352ee0d9efd81e1272448b
zelitosaide <zelitosaide83@gmail.com>
2023-02-27 10:54:09 +0200

    Document

--- part-2-browser-document-events-interfaces/document/attributes-and-properties/index.js
+++ part-2-browser-document-events-interfaces/document/attributes-and-properties/index.js
@@ -8,3 +8,7 @@ console.log(document.body.myData.title);
 document.body.sayTagName = function () {
   console.log(this.tagName);
 };
+
+Element.prototype.printTagName = function () {
+  console.log(`Hello, I'm ${this.tagName}`);
+};
