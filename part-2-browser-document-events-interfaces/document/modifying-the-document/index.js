1f4c26071be1199c0fadb429e5533eba1a7bc54c
zelitosaide <zelitosaide83@gmail.com>
2023-02-28 08:04:49 +0200

    Document

--- part-2-browser-document-events-interfaces/document/modifying-the-document/index.js
+++ part-2-browser-document-events-interfaces/document/modifying-the-document/index.js
@@ -1,2 +1,10 @@
+// 1. Create <div> element
 const div = document.createElement("div");
+
+// 2. Set its class to "alert"
+div.className = "alert";
+
+// 3. Fill it with the content
+div.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
+
 const textNode = document.createTextNode("Here I am");
