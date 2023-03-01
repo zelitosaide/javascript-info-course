2e64aaadd97fbe7f21fe2dc522c9ae0f8048b229
zelitosaide <zelitosaide83@gmail.com>
2023-03-01 20:35:25 +0200

    Document

---
+++ part-2-browser-document-events-interfaces/document/modifying-the-document/index2.js
@@ -0,0 +1,5 @@
+// Creating an element
+// document.createElement(tag)
+// const div = document.createElement('div')
+// document.createTextNode(text)
+// const textNode = document.createTextNode('Here I am')
