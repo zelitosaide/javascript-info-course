eec9b2d200a0c1546068013c16927ab0ec41eb7c
zelitosaide <zelitosaide83@gmail.com>
2023-03-12 14:14:45 +0200

    DOCUMENT

--- part-1-the-javascript-language/advenced-working-with-functions/scheduling-settimeout-and-setinterval/zero-delay-setImmediate.js
+++ part-1-the-javascript-language/advenced-working-with-functions/scheduling-settimeout-and-setinterval/zero-delay-setImmediate.js
@@ -2,11 +2,11 @@ const start = Date.now();
 let times = [];
 
 let timerId = setImmediate(function run() {
-  times.push();
+  times.push(Date.now() - start);
 
   if (start + 100 < Date.now()) {
     console.log(times);
+  } else {
+    timerId = setImmediate(run);
   }
-
-  timerId = setImmediate(run);
 });
