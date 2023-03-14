b68a8652904c6dbfe1ac3b702352d5e8466ef397
zelitosaide <zelitosaide83@gmail.com>
2023-03-14 18:32:45 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/styles-and-classes/index.js
+++ part-2-browser-document-events-interfaces/document/styles-and-classes/index.js
@@ -7,3 +7,9 @@ elem.style.top = top;
 elem.style.left = left;
 
 // className and classList
+
+// elem.className
+// If we assign something to elem.className, it replaces the whole string of classes.
+
+// elem.classList
+// The elem.classList is a special object with methods to add/remove/toggle a single class.
