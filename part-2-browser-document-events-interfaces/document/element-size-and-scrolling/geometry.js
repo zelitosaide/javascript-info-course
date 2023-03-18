eb698e8c985d44ebf8acc9f3827f05815cab6a72
zelitosaide <zelitosaide83@gmail.com>
2023-03-18 11:54:08 +0200

    DOCUMENT

---
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/geometry.js
@@ -0,0 +1,6 @@
+/**
+ * That’s the nearest ancestor that is one of the following:
+ * 1 - CSS-positioned (position is absolute, relative, fixed or sticky), or
+ * 2 - <td>, <th>, or <table>, or
+ * 3 - <body>.
+ */
