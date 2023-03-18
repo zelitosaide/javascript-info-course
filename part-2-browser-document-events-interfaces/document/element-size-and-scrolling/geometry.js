e3a1bc2d8424856e39d64f643ad6464a05c5429a
zelitosaide <zelitosaide83@gmail.com>
2023-03-18 18:00:36 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/geometry.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/geometry.js
@@ -89,3 +89,5 @@ console.log("parent", elem4.offsetTop);
 console.log("parent", elem4.offsetLeft);
 
 // case with parent not added to document
+const elem5 = document.createElement("div");
+elem5.style.position = "relative";
