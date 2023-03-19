02c37a17f3fed53e9cc2531e84f75dea10b43d9d
zelitosaide <zelitosaide83@gmail.com>
2023-03-19 09:03:17 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/scroll-width-height.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/scroll-width-height.js
@@ -1,3 +1,7 @@
 // These properties are like clientWidth/clientHeight, but they also include the scrolled out (hidden) parts:
 // scrollHeight = 723 – is the full inner height of the content area including the scrolled out parts.
 // scrollWidth = 324 – is the full inner width, here we have no horizontal scroll, so it equals clientWidth.
+
+// scrollLeft/scrollTop
+// Properties scrollLeft/scrollTop are the width/height of the hidden, scrolled out part of the element.
+// In other words, scrollTop is “how much is scrolled up”.
