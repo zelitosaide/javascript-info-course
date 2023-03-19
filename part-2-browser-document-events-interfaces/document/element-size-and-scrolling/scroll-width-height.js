8d768016409781b733769369f18bc05b9308d79b
zelitosaide <zelitosaide83@gmail.com>
2023-03-19 08:31:18 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/scroll-width-height.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/scroll-width-height.js
@@ -1,1 +1,3 @@
 // These properties are like clientWidth/clientHeight, but they also include the scrolled out (hidden) parts:
+// scrollHeight = 723 – is the full inner height of the content area including the scrolled out parts.
+// scrollWidth = 324 – is the full inner width, here we have no horizontal scroll, so it equals clientWidth.
