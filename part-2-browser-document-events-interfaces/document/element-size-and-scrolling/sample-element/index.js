7b27440c4b6678f86f1c65ae0c1f9ab4fd65bf33
zelitosaide <zelitosaide83@gmail.com>
2023-03-21 20:39:28 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/sample-element/index.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/sample-element/index.js
@@ -1,2 +1,8 @@
 const example = document.getElementById("example");
-console.log(example);
+const styles = getComputedStyle(example);
+const width = +styles.width.split("px")[0];
+const padding = +styles.paddingLeft.split("px")[0];
+const clientWidthExperimental = +styles.width.split("px")[0];
+const clientWidth = "content width (width + padding - scrollbar)";
+
+console.log(clientWidth, example.clientWidth, padding);
