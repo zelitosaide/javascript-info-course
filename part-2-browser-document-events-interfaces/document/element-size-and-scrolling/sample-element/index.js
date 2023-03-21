e9fc9fd838b6f966b7bc399565a193f652039552
zelitosaide <zelitosaide83@gmail.com>
2023-03-21 22:01:20 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/sample-element/index.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/sample-element/index.js
@@ -12,6 +12,6 @@ const borderLeft = example.clientLeft;
 const borderTop = example.clientTop;
 
 console.log(clientWidth, example.clientWidth, clientWidthExperimental);
-console.log(clientHeight, example.clientHeight);
+console.log(clientHeight, example.clientHeight, clientHeightExperimental);
 console.log("border left", example.clientLeft);
 console.log("border top", borderTop);
