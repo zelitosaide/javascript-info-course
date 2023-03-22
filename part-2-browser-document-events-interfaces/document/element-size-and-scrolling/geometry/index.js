df862b517e6e3fde6602812b38133931f76a8fad
zelitosaide <zelitosaide83@gmail.com>
2023-03-22 12:55:51 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/geometry/index.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/geometry/index.js
@@ -6,6 +6,6 @@ console.log("offsetLeft", example.offsetLeft);
 console.log("clientLeft", example.clientLeft);
 console.log("clientTop", example.clientTop);
 console.log("clientWidth", example.clientWidth, 20 * 2 + 283);
-console.log("clientHeight", example.clientHeight);
+console.log("clientHeight", example.clientHeight, 20 * 2 + 200);
 
 console.log(styles.width);
