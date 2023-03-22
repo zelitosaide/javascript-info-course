ff890ec30ad3b3c998a3f9443c726873e72b7bbe
zelitosaide <zelitosaide83@gmail.com>
2023-03-22 13:40:24 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/offset-parent-offset-left-offset-top/index.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/offset-parent-offset-left-offset-top/index.js
@@ -2,3 +2,4 @@ const example = document.getElementById("example");
 
 console.log("offsetParent", example.offsetParent);
 console.log("offsetLeft", example.offsetLeft);
+console.log("offsetTop", example.offsetTop);
