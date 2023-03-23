24f4559f777fbddeb70406f7869ac822c9cff752
zelitosaide <zelitosaide83@gmail.com>
2023-03-23 21:28:48 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/client-top-and-client-left/index.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/client-top-and-client-left/index.js
@@ -1,2 +1,8 @@
 // …But to be precise – these properties are not border width/height, but
 // rather relative coordinates of the inner side from the outer side.
+
+// What’s the difference?
+// It becomes visible when the document is right-to-left (the operating
+// system is in Arabic or Hebrew languages). The scrollbar is then not on
+// the right, but on the left, and then clientLeft also includes the
+// scrollbar width.
