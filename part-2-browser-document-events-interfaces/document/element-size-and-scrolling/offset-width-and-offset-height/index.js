07b638ed9b5dae7f9ccdd1337756b8ffb1c52ef5
zelitosaide <zelitosaide83@gmail.com>
2023-03-23 19:49:46 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/offset-width-and-offset-height/index.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/offset-width-and-offset-height/index.js
@@ -2,3 +2,9 @@ const example = document.getElementById("example");
 
 console.log("offsetWidth", example.offsetWidth, 250 + 2 * 25 + 2 * 20);
 console.log("offsetHeight", example.offsetHeight, 150 + 2 * 25 + 2 * 20);
+
+// Geometry properties are zero/null for elements that are not displayed
+// Geometry properties are calculated only for displayed elements.
+// If an element (or any of its ancestors) has display:none or is not in
+// the document, then all geometry properties are zero (or null for
+// offsetParent).
