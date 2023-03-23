69914b55b6f37e826e82bc5d66c37c7f0209373d
zelitosaide <zelitosaide83@gmail.com>
2023-03-23 19:53:10 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/offset-width-and-offset-height/index.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/offset-width-and-offset-height/index.js
@@ -13,3 +13,4 @@ console.log("offsetHeight", example.offsetHeight, 150 + 2 * 25 + 2 * 20);
 // when we created an element, but haven’t inserted it into the document
 // yet, or it (or its ancestor) has display:none.
 const notInTheDOM = document.createElement("div");
+console.log("offsetParent", notInTheDOM.offsetParent);
