9d5b19bbaa487ccd46f2b08728ae4dead6369e4f
zelitosaide <zelitosaide83@gmail.com>
2023-03-24 21:01:44 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/client-width-and-client-height/index.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/client-width-and-client-height/index.js
@@ -1,2 +1,5 @@
 // If there are no paddings, then clientWidth/Height is exactly the
 // content area, inside the borders and the scrollbar (if any).
+
+// So when there’s no padding we can use clientWidth/clientHeight to get
+// the content area size.
