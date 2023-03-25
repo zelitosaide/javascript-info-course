df1d09943ed2eeca06891a1d038e679eba218279
zelitosaide <zelitosaide83@gmail.com>
2023-03-25 16:45:35 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/scroll-left-and-scroll-top/index.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/scroll-left-and-scroll-top/index.js
@@ -4,3 +4,8 @@ function showScrollTop() {
   console.log("scrollTop", example.scrollTop);
   console.log("scrollLeft", example.scrollLeft);
 }
+
+// scrollLeft/scrollTop can be modified
+// Most of the geometry properties here are read-only, but
+// scrollLeft/scrollTop can be changed, and the browser will scroll the
+// element.
