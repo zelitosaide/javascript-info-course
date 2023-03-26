1640eafdfbe93a572c7fa181c02ef8ad56b09aee
zelitosaide <zelitosaide83@gmail.com>
2023-03-26 07:59:03 +0200

    DOCUMENT

--- part-2-browser-document-events-interfaces/document/element-size-and-scrolling/do-not-take-width-height-from-css/index.js
+++ part-2-browser-document-events-interfaces/document/element-size-and-scrolling/do-not-take-width-height-from-css/index.js
@@ -1,1 +1,8 @@
 // Don’t take width/height from CSS
+// So why not to read the width of an element with getComputedStyle, like
+// this?
+// Why should we use geometry properties instead? There are two reasons:
+// 1. First, CSS width/height depend on another property: box-sizing that
+//    defines “what is” CSS width and height. A change in box-sizing for CSS
+//    purposes may break such JavaScript.
+// 2. Second, CSS width/height may be auto, for instance for an inline element:
